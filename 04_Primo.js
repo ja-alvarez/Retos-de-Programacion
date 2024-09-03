2294df578a401d0a540db1fc425b2cc28a24b716
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-03 19:47:42 -0400

    Numeros primos

---
+++ 04_Primo.js
@@ -0,0 +1,18 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+const primo = (n) => {
+    console.log(n)
+    if (n>1){
+        for(let i = 2; i < n; i++) {
+            if (n % i === 0) return false;
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(primo(17))
